642240a4f8b95579ce4d5e71822a002e49e741b9
Abdelrhman Ayman <adodoadodo2025@gmail.com>
2026-07-12 17:29:10 +0300

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+
+        int canPlant = 0;
+
+        for (int i = 1; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                canPlant++;
+                flowerbed[i] = 1;
+            }
+        }
+
+        return canPlant >= n;
+    }
+};
