527b7c5bbfe4484830d44981226a4d606ec697e5
Abdelrhman Ayman <adodoadodo2025@gmail.com>
2026-07-13 23:37:49 +0300

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        for (int i = 1; i < x / 2; i++) {
+            if (i * i == x) return i;
+
+            if (i * i > x) return i - 1;
+        }
+    }
+};
